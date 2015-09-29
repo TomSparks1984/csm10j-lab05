6657a26eb1260428bd17570d555fe39dbc62b73b
Thomas Sparks <TomSparks1984@yahoo.com>
2015-09-29 08:21:31 -0700

    Update Lab05.java

--- Lab05.java
+++ Lab05.java
@@ -15,72 +15,54 @@ public class Lab05 {
         numTwo = console.next();
         
         int biggestLength;
+        String temp = "";
         
         if (numOne.length() > numTwo.length()){
             biggestLength = numOne.length();
         }else {
             biggestLength = numTwo.length();
+            temp = numOne;
+            numOne = numTwo;
+            numTwo = temp;
+        }
+        while (numTwo.length() < numOne.length())
+        {
+            numTwo = "0" + numTwo;
         }
         
         int[] numOneDigits = new int[numOne.length()];
-        int[] numTwoDigits = new int[numTwo.length()];
+        int[] numTwoDigits = new int[numOne.length()];
         
-        int j = 0;
-        for(int i = numOne.length() - 1; i >= 0; i--){
-            numOneDigits[j] = Character.getNumericValue(numOne.charAt(i));
-            j++;
+        for(int i = 0; i < numOne.length(); i++){
+            numOneDigits[i] = Character.getNumericValue(numOne.charAt(i));
         }
-        j = 0;
-        for(int i = 0; i < numTwo.length(); i++){
-            numTwoDigits[j] = Character.getNumericValue(numTwo.charAt(i));
-            j++;
+        for(int i = 0; i < numOne.length(); i++){
+            if (i < numTwo.length())
+            {            
+                numTwoDigits[i] = Character.getNumericValue(numTwo.charAt(i));
+            }
         }
         sum(numOneDigits, numTwoDigits, biggestLength);
     }
     public static void sum(int[] numOneDigits, int [] numTwoDigits, int biggestLength) {
         int remainder = 0;
-        int [] sumDigits = new int[biggestLength+1];
-        
-        for(int i = 0; i < biggestLength; i++){
-            if(i + 1 > numOneDigits.length){
-                sumDigits[i] = numTwoDigits[i] + remainder;
-                if(numTwoDigits[i] + remainder > 9){
-                    remainder = 1;
-                } else{
-                    remainder = 0;
-                }    
-            } else if (i + 1 > numTwoDigits.length) {
-                sumDigits[i] = numOneDigits[i] + remainder;
-                if(numOneDigits[i] + remainder > 9){
-                    remainder = 1;
-                } else{
-                    remainder = 0;
-                }    
-            } else {
-                if(numOneDigits[i] + numTwoDigits[i] > 9){
-                    sumDigits[i] = numOneDigits[i] + numTwoDigits[i] - 10 + remainder;
-                    remainder = 1;
-                } else{
-                    sumDigits[i] = numOneDigits[i] + numTwoDigits[i] + remainder; 
-                    remainder = 0;
-                }    
-            }
+        int [] sumDigits = new int[biggestLength];
+        int sum;
+
+        for (int i = numTwoDigits.length - 1; i >= 0; i--)
+        {
+            sum = numOneDigits[i] + numTwoDigits[i] + remainder;
+            sumDigits[i] = sum % 10;
+            remainder = (sum - sumDigits[i]) / 10;
         }
-        if (remainder == 1){
-            sumDigits[biggestLength] = 1;
-        }else {
-            sumDigits[biggestLength] = 0;    
+        if (remainder > 0)
+        {
+            System.out.print(remainder);
         }
-        for (int i = 0; i < sumDigits.length; i ++) {
-            System.out.println(sumDigits[i]);           
+        for (int i = 0; i < sumDigits.length; i++)
+        {
+            System.out.print(sumDigits[i]);
         }
-        String sumString = "";
-        
-        for (int i = sumDigits.length - 1; i >= 0; i--){
-            Integer.toString()
-            sumDigits[i]
-                
-        }
-        System.out.println(sumString);
+        System.out.println();
     }
 }
